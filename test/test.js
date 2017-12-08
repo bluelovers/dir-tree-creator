f9741324792c0b6e43c8387dd29f1e38e4b972bf
Dan Florio <dflo@danflorio.com>
2017-12-08 15:58:21 -0500

    add option to ignore hidden files

--- test/test.js
+++ test/test.js
@@ -52,10 +52,47 @@ describe('dir-tree-creator', () => {
     └─┬ dir333
       └── f4`
 
-    dirTree(testDir, 'custom-label', (er, tr) => {
+    dirTree(testDir, { label: 'custom-label' }, (er, tr) => {
       if (er) assert.ifError(er)
       assert.strictEqual(tr, wanted)
       done()
     })
   })
+
+  // TODO: Rewrite test to also work on Windows systems. For now it doesn't
+  // run if testing on Windows.
+  if (process.platform !== 'win32') {
+    it('should return string representation of dir tree structure - no hidden files', done => {
+      fs.ensureFileSync(path.join(testDir, 'dir1', 'f1'))
+      fs.ensureFileSync(path.join(testDir, 'dir2', 'dir22', 'f2'))
+      fs.ensureFileSync(path.join(testDir, 'dir2', 'dir22', 'f3'))
+      fs.ensureFileSync(path.join(testDir, 'dir3', 'dir33', 'dir333', 'f4'))
+      fs.ensureFileSync(path.join(testDir, '.dir4', 'f5'))
+      fs.ensureFileSync(path.join(testDir, '.dir5', '.f6'))
+      fs.ensureFileSync(path.join(testDir, 'dir6', '.f7'))
+      fs.ensureFileSync(path.join(testDir, 'dir6', 'f8'))
+      fs.ensureFileSync(path.join(testDir, 'dir7', '.dir77', 'f9'))
+
+      const wanted = `dir-tree-creator
+├─┬ dir1
+│ └── f1
+├─┬ dir2
+│ └─┬ dir22
+│   ├── f2
+│   └── f3
+├─┬ dir3
+│ └─┬ dir33
+│   └─┬ dir333
+│     └── f4
+├─┬ dir6
+│ └── f8
+└── dir7`
+
+      dirTree(testDir, { hidden: false }, (er, tr) => {
+        if (er) assert.ifError(er)
+        assert.strictEqual(tr, wanted)
+        done()
+      })
+    })
+  }
 })
